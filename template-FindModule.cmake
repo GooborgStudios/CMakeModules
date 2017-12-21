540ffe2cf79553ced94db6966de203396f297726
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-12-21 01:25:48 -0800

    Make output look better

--- template-FindModule.cmake
+++ template-FindModule.cmake
@@ -4,11 +4,10 @@
 # 
 # When creating a new find module to add to this repository, use the following format.
 # Replace "TemplateLib" with the name of the library.
-# Replace "TEMPLATE_LIB" with the name of the library in all caps, using underscores as appropriate.
 # Update paths and files to look for as appropriate.
 # If the library has multiple parts (i.e. how GraphicsMagick has Magick, Magick++, and MagickWand), copy the calls and modify message as appropriate.
 #
-# Make sure to remove these top thirteen lines before submitting a PR!
+# Make sure to remove these top twelve lines before submitting a PR!
 #
 ##########
 
@@ -20,25 +19,22 @@
 # Search for TemplateLib library files
 #
 # Once loaded this will define:
-#   TEMPLATE_LIB_FOUND       - system has TemplateLib
-#   TEMPLATE_LIB_INCLUDE_DIR - include directory for TemplateLib
-#   TEMPLATE_LIB_LIBRARY_DIR - library directory for TemplateLib
-#   TEMPLATE_LIB_LIBRARIES   - libraries you need to link to
+#   TemplateLib_FOUND       - system has TemplateLib
+#   TemplateLib_INCLUDE_DIR - include directory for TemplateLib
+#   TemplateLib_LIBRARY_DIR - library directory for TemplateLib
+#   TemplateLib_LIBRARIES   - libraries you need to link to
 
 
-
-# Initialize _FOUND variable
-SET(TEMPLATE_LIB_FOUND "NO")
 
 # Find headers
-FIND_PATH(TEMPLATE_LIB_INCLUDE_DIR
+FIND_PATH(TemplateLib_INCLUDE_DIR
 	NAMES "TemplateLib/TemplateLib.h"
 	PATHS
 	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\TemplateLib\\Current;BinPath]"
-	"$ENV{TEMPLATE_LIB_LOCATION}"
-	"$ENV{TEMPLATE_LIB_LOCATION}/include"
-	"$ENV{TEMPLATE_LIB_LOCATION}/include/TemplateLib"
-	"$ENV{TEMPLATE_LIB_HOME}/include"
+	"$ENV{TemplateLib_LOCATION}"
+	"$ENV{TemplateLib_LOCATION}/include"
+	"$ENV{TemplateLib_LOCATION}/include/TemplateLib"
+	"$ENV{TemplateLib_HOME}/include"
 	/usr/include/
 	/usr/include/TemplateLib
 	/usr/local/include
@@ -47,13 +43,13 @@ FIND_PATH(TEMPLATE_LIB_INCLUDE_DIR
 )
 
 # Find compiled library file
-FIND_LIBRARY(TemplateLib
+FIND_LIBRARY(TemplateLib_LIBRARIES
 	NAMES libTemplateLib TemplateLib
 	PATHS 
 	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\TemplateLib\\Current;BinPath]"
-	"$ENV{TEMPLATE_LIB_LOCATION}/TEMPLATE_LIB/.libs"
-	"$ENV{TEMPLATE_LIB_LOCATION}/lib"
-	"$ENV{TEMPLATE_LIB_HOME}/lib"
+	"$ENV{TemplateLib_LOCATION}/TemplateLib/.libs"
+	"$ENV{TemplateLib_LOCATION}/lib"
+	"$ENV{TemplateLib_HOME}/lib"
 	/usr/lib64
 	/usr/local/lib64
 	/opt/local/lib64
